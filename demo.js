38450509cb7e7a0bc6d3b93721e95e64e9901407
maaranmobiles <123291987+maaranmobiles@users.noreply.github.com>
2024-04-27 00:56:45 +0530

    Modified by Kavinmozhi

--- demo.js
+++ demo.js
@@ -1,3 +1,3 @@
 echo "function example() {" > example.js
-echo "  console.log('original code');" >> example.js
+echo "  console.log('code changed by kavinmozhi');" >> example.js
 echo "}" >> example.js
